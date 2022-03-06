db67587d1484fd44a9936c0ed3869f2c678f47fa
OdhranButler <91945585+OdhranButler@users.noreply.github.com>
2022-03-06 18:25:03 +0000

    Add files via upload

--- insert.php
+++ insert.php
@@ -1,6 +1,13 @@
-<html>
-    <body>
-        <?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+
+</head>
+
+<body>
+    <?php
             include 'db.inc.php';
             date_default_timezone_set("UTC");
             echo "The details sent down are: <br>";
@@ -9,27 +16,27 @@
             echo "Surname is : " . $_POST['surname'] . "<br>";
             echo "EmailAddress is : " . $_POST['emailaddress'] . "<br>";
             echo "PhoneNumber is : " . $_POST['phonenumber'] . "<br>";
+            echo "Date of birth is : " . $_POST['dob'] . "<br>";
 
             $date=date_create($_POST['dob']);
 
-	        echo "Date of Birth is :" . date_format($date,"d/m/Y") . "<br>";
-            $sql = "Insert into PersonID ('firstname,surname,email,phonenumber,dob')
-            VALUES ('$_POST[firstname]','$_POST[surname]','$_POST[emailaddress]','$_POST[phonenumber]','$_POST[dob]')";
-			
-            if (!mysqli_query($con,$sql))
+            $sql = "Insert into persons (firstname,lastname,emailaddress,phonenumber,DOB)
+            Values ('$_POST[firstname]','$_POST[surname]','$_POST[emailaddress]','$_POST[phonenumber]','$_POST[dob]')";
+
+            if(!mysqli_query($con,$sql))
             {
                 die ("An Error in the SQL Query: " . mysqli_error($con));
             }
-			
             echo "<br>A record has been added for  " . $_POST['firstname'] . " " . $_POST['surname'] . " " . $_POST['emailaddress'] . " " . $_POST['phonenumber'] . ".";
 
             mysqli_close($con);
 
         ?>
 
-        <form action = "insert.html" method = "POST" >
-            <br>
-                <input type="submit" value = "Return to Insert Page"/>
-        </form>
-    </body>
+    <form action="insert.html" method="POST">
+        <br>
+        <input type="submit" value="Return to Insert Page" />
+    </form>
+</body>
+
 </html>
