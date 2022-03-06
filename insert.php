19b6cd8d30b5a1aa7d7a8d19276578a16f544e49
OdhranButler <91945585+OdhranButler@users.noreply.github.com>
2022-03-06 18:24:40 +0000

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,35 @@
+<html>
+    <body>
+        <?php
+            include 'db.inc.php';
+            date_default_timezone_set("UTC");
+            echo "The details sent down are: <br>";
+
+            echo "First Name is : " . $_POST['firstname'] . "<br>";
+            echo "Surname is : " . $_POST['surname'] . "<br>";
+            echo "EmailAddress is : " . $_POST['emailaddress'] . "<br>";
+            echo "PhoneNumber is : " . $_POST['phonenumber'] . "<br>";
+
+            $date=date_create($_POST['dob']);
+
+	        echo "Date of Birth is :" . date_format($date,"d/m/Y") . "<br>";
+            $sql = "Insert into PersonID ('firstname,surname,email,phonenumber,dob')
+            VALUES ('$_POST[firstname]','$_POST[surname]','$_POST[emailaddress]','$_POST[phonenumber]','$_POST[dob]')";
+			
+            if (!mysqli_query($con,$sql))
+            {
+                die ("An Error in the SQL Query: " . mysqli_error($con));
+            }
+			
+            echo "<br>A record has been added for  " . $_POST['firstname'] . " " . $_POST['surname'] . " " . $_POST['emailaddress'] . " " . $_POST['phonenumber'] . ".";
+
+            mysqli_close($con);
+
+        ?>
+
+        <form action = "insert.html" method = "POST" >
+            <br>
+                <input type="submit" value = "Return to Insert Page"/>
+        </form>
+    </body>
+</html>
